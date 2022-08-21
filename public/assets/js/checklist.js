4abfabbf512f2e61a8b383daf5a1546833b0d91d
Lovlesh Singh <lovleshsingh99@gmail.com>
2022-08-22 02:14:05 +0530

    commit

--- public/assets/js/checklist.js
+++ public/assets/js/checklist.js
@@ -81,9 +81,9 @@ $('#ac-timer').text(localStorage.getItem('actimertype'))
 $('#atm-note').text(localStorage.getItem('atm_note'))
 var html = '<img src="' + localStorage.getItem('signimage') + '"' + 'alt="" class="img-fluid">'
 
-document.getElementById('whatever').innerHTML = html
-
+document.getElementById('sign-image').innerHTML = html
 $('.btn-createpdf').css('display', 'block')
+
 function encodeImageFileAsURL(element) {
     var file = element.files[0];
     var reader = new FileReader();
@@ -93,76 +93,94 @@ function encodeImageFileAsURL(element) {
     reader.readAsDataURL(file);
 }
 
+
+function encodeImageFileAsURL2(element) {
+    var fileArray = [];
+    for(var i = 0; i < element.files.length; i++){
+        var file = element.files[i];
+        var reader = new FileReader();
+        reader.onloadend = function (){
+            fileArray.push(this.result)
+        }
+        reader.readAsDataURL(file)
+    }    
+    console.log(fileArray[0])
+}
+
 function createPDF(){
-    $('.btn-createpdf').css('display', 'none')
+        $('.btn-createpdf').css('display', 'none')
 
-    var element = document.getElementById('pf1');
-    var opt = {
-        margin:       1,
-        filename:     localStorage.getItem('atm_id_dropdown') + ' ' + localStorage.getItem('atm_city_dropdown') + ' ' + localStorage.getItem('datentime1'),
-        image:        { type: 'jpeg', quality: 1 },
-        html2canvas:  {scale:5},
-        jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' },
-        margin:       [0,0,0,0]
-        };
+        var element = document.getElementById('pf1');
+        var opt = {
+            margin:       0,
+            filename:     localStorage.getItem('atm_id_dropdown') + ' ' + localStorage.getItem('atm_city_dropdown') + ' ' + localStorage.getItem('datentime1'),
+            image:        { type: 'jpeg', quality: 1 },
+            html2canvas:  {scale:5},
+            jsPDF:        { unit: 'mm', format:[170,330], orientation: 'portrait' },
+            };
 
-    html2pdf(element, opt)
+        html2pdf(element, opt).save()
 
-    setTimeout(() => {
-        window.location = 'index.html'
-    }, 3000);
-
+        setTimeout(() => {
+            // window.location = 'index.html'
+        }, 3000);
 }
 
 function storedata(){
-    localStorage.setItem('datentime1', $("#datetime").val())
-    localStorage.setItem('atm_city_dropdown', $('#atm_city_dropdown').val());
-    localStorage.setItem('atm_id_dropdown', $('#atm_id_dropdown').val());
-    localStorage.setItem('atm_name_dropdown', $('#atm_name_dropdown').val());
-    localStorage.setItem('atm_manager_name', $('#atm_manager_name').val());
-    localStorage.setItem('atm_contact', $('#atm_contact').val());
-    localStorage.setItem('atm_email', $('#atm_email').val());
-    localStorage.setItem('atm_person_name', $('#atm_person_name').val());
-    localStorage.setItem('atm_person_contact', $('#atm_person_contact').val());
+    if ($('#atm_sign_file')[0].files.length === 0) {
+        alert("Please add Signature");
+        return false;
+    } else {
+        localStorage.setItem('datentime1', $("#datetime").val())
+        localStorage.setItem('atm_city_dropdown', $('#atm_city_dropdown').val());
+        localStorage.setItem('atm_id_dropdown', $('#atm_id_dropdown').val());
+        localStorage.setItem('atm_name_dropdown', $('#atm_name_dropdown').val());
+        localStorage.setItem('atm_manager_name', $('#atm_manager_name').val());
+        localStorage.setItem('atm_contact', $('#atm_contact').val());
+        localStorage.setItem('atm_email', $('#atm_email').val());
+        localStorage.setItem('atm_person_name', $('#atm_person_name').val());
+        localStorage.setItem('atm_person_contact', $('#atm_person_contact').val());
 
-    localStorage.setItem('tac1', $('#tac1').val());
-    localStorage.setItem('tac2', $('#tac2').val());
-    localStorage.setItem('mac1', $('#mac1').val());
-    localStorage.setItem('mac2', $('#mac2').val());
-    localStorage.setItem('cac1', $('#cac1').val());
-    localStorage.setItem('cac2', $('#cac2').val());
-    localStorage.setItem('coac1', $('#coac1').val());
-    localStorage.setItem('coac2', $('#coac2').val());
-    localStorage.setItem('dac1', $('#dac1').val());
-    localStorage.setItem('dac2', $('#dac2').val());
-    localStorage.setItem('mups', $('#mups').val());
-    localStorage.setItem('caups', $('#caups').val());
-    localStorage.setItem('bups', $('#bups').val());
-    localStorage.setItem('avrups', $('#avrups').val());
-    localStorage.setItem('transups', $('#transups').val());
-    localStorage.setItem('servosups', $('#servosups').val());
-    localStorage.setItem('volsups', $('#volsups').val());
-    localStorage.setItem('sldelecsys', $('#sldelecsys').val());
-    localStorage.setItem('panelselecsys', $('#panelselecsys').val());
-    localStorage.setItem('tlobby', $('#tlobby').val());
-    localStorage.setItem('nlobby', $('#nlobby').val());
-    localStorage.setItem('dsignage', $('#dsignage').val());
-    localStorage.setItem('timesignage', $('#timesignage').val());
-    localStorage.setItem('mdvr', $('#mdvr').val());
-    localStorage.setItem('cameradvr', $('#cameradvr').val());
-    localStorage.setItem('hdddvr', $('#hdddvr').val());
-    localStorage.setItem('ddoor', $('#ddoor').val());
-    localStorage.setItem('ddoorstopper', $('#ddoorstopper').val());
-    localStorage.setItem('atmcount', $('#atmcount').val());
-    localStorage.setItem('siteimage', $('#siteimage').val());
-    localStorage.setItem('metereb', $('#metereb').val());
-    localStorage.setItem('typeeb', $('#typeeb').val());
-    localStorage.setItem('stimertype', $('#stimertype').val());
-    localStorage.setItem('actimertype', $('#actimertype').val());
-    localStorage.setItem('atm_note', $("#atm_note").val())
-    
-    window.location = 'atmchecklist.html'
+        localStorage.setItem('tac1', $('#tac1').val());
+        localStorage.setItem('tac2', $('#tac2').val());
+        localStorage.setItem('mac1', $('#mac1').val());
+        localStorage.setItem('mac2', $('#mac2').val());
+        localStorage.setItem('cac1', $('#cac1').val());
+        localStorage.setItem('cac2', $('#cac2').val());
+        localStorage.setItem('coac1', $('#coac1').val());
+        localStorage.setItem('coac2', $('#coac2').val());
+        localStorage.setItem('dac1', $('#dac1').val());
+        localStorage.setItem('dac2', $('#dac2').val());
+        localStorage.setItem('mups', $('#mups').val());
+        localStorage.setItem('caups', $('#caups').val());
+        localStorage.setItem('bups', $('#bups').val());
+        localStorage.setItem('avrups', $('#avrups').val());
+        localStorage.setItem('transups', $('#transups').val());
+        localStorage.setItem('servosups', $('#servosups').val());
+        localStorage.setItem('volsups', $('#volsups').val());
+        localStorage.setItem('sldelecsys', $('#sldelecsys').val());
+        localStorage.setItem('panelselecsys', $('#panelselecsys').val());
+        localStorage.setItem('tlobby', $('#tlobby').val());
+        localStorage.setItem('nlobby', $('#nlobby').val());
+        localStorage.setItem('dsignage', $('#dsignage').val());
+        localStorage.setItem('timesignage', $('#timesignage').val());
+        localStorage.setItem('mdvr', $('#mdvr').val());
+        localStorage.setItem('cameradvr', $('#cameradvr').val());
+        localStorage.setItem('hdddvr', $('#hdddvr').val());
+        localStorage.setItem('ddoor', $('#ddoor').val());
+        localStorage.setItem('ddoorstopper', $('#ddoorstopper').val());
+        localStorage.setItem('atmcount', $('#atmcount').val());
+        localStorage.setItem('siteimage', $('#siteimage').val());
+        localStorage.setItem('metereb', $('#metereb').val());
+        localStorage.setItem('typeeb', $('#typeeb').val());
+        localStorage.setItem('stimertype', $('#stimertype').val());
+        localStorage.setItem('actimertype', $('#actimertype').val());
+        localStorage.setItem('atm_note', $("#atm_note").val())
+        
+        window.location = 'atmchecklist.html'
+    }
 }
+
 function reset(){
     location.reload()
 }
